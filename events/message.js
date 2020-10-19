2e156291a301761135fd80d12ee4ae82b4e9d1fe
daniel5234 <55165609+daniel5234@users.noreply.github.com>
2020-10-18 17:52:18 -0700

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,25 @@
+//const { prefix } = require('../config.js');
+
+/*module.exports = (client, msg) => {
+    if (msg.author.bot) return;
+    if (msg.content.indexOf('im')!== 0) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    //const command = args.shift().toLowerCase();
+    const message = args.join(' ');
+
+    msg.channel.send(`Hi, ${message}`);
+};*/
+
+module.exports = (client, msg) => {
+    if (msg.author.bot) return;
+    if (msg.content.indexOf(process.env.PREFIX)!== 0) return;
+
+    const args = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const cmd = client.commands.get(command);
+    if (!cmd) return;
+
+    cmd.run(client, msg, args);
+};
