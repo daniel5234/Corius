6517377a13ce526df659ffd1f33448796c0d4e01
daniel5234 <55165609+daniel5234@users.noreply.github.com>
2020-11-02 12:20:14 -0800

    Update woosh.js

--- commands/woosh.js
+++ commands/woosh.js
@@ -1,6 +1,6 @@
 exports.run = (client, msg, args) => {
     //if(!msg.member.roles.cache.has('753107491490889790'))return;
-    var thingy = "```\n                                     /———————————>\n                                   /\n                                 /                                  GET R/WOOSHED\n                               /                   you\n                  the joke ---/                    O\n                                                  /|\\n                                                  /\   \n```"
+    var thingy = "```\n                                     /———————————>\n                                   /\n                                 /                                  GET R/WOOSHED\n                               /                   you\n                  the joke ---/                    O\n                                                  /|\\\n                                                  /\\   \n```"
     msg.channel.send(thingy).catch(console.error);
 };
 
