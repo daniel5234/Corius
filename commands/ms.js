6690bb90f6126465092b59a3c7ecadd6dd69c246
daniel5234 <55165609+daniel5234@users.noreply.github.com>
2020-11-07 11:46:57 -0800

    Update ms.js

--- commands/ms.js
+++ commands/ms.js
@@ -3,15 +3,22 @@ const Minesweeper = require('discord.js-minesweeper');
 
 
 exports.run = (client, msg, args) => {
+	
+	const row = args[0];
+	const column = args[1];
+	const mine = args[2];
+	if (!row||!column||!mine) {
+		return msg.channel.send(`!ms <rows> <columns> <mines>`)
+	}
 
     const minesweeper = new Minesweeper({
-    	rows: 8,
-    	columns: 8,
-    	mines: 8,
+    	rows: row,
+    	columns: column,
+    	mines: mine,
     	emote: 'boom',
-	spaces: true,
+		spaces: true,
     	zeroFirstCell: true,
-	revealFirstCell: false,
+		revealFirstCell: false,
     	returnType: 'emoji',
 	});
 	var board = minesweeper.start();
