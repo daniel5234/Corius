d5e4c65d7c41c1268cfb143eca228072d4f097da
daniel5234 <55165609+daniel5234@users.noreply.github.com>
2020-11-10 10:40:45 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 exports.run = (client, msg, args) => {
-    if(!msg.member.roles.cache.has('753107491490889790'))return;
-    msg.channel.send('Pong!').catch(console.error);
+    //if(!msg.member.roles.cache.has('753107491490889790'))return;
+    //msg.channel.send('Pong!').catch(console.error);
+    msg.channel.send(`Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 };
 
 exports.help = {
