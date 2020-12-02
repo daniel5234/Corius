f87cec45fbc77a54ca4e99b1a42e712b2fa61076
daniel5234 <55165609+daniel5234@users.noreply.github.com>
2020-12-02 09:03:30 -0800

    Update ttspam.js

--- commands/ttspam.js
+++ commands/ttspam.js
@@ -1,9 +1,11 @@
 exports.run = (client, msg, args) => {
-    if(!msg.member.roles.cache.has('753107491490889790'))return;
+    if(!(msg.member.roles.cache.has('753107491490889790'))||(msg.member.roles.cache.has('754062730532618311')))return;
     let num = args.shift();
     let sendmessage=args.join(" ");
 
     //if(args[0]=='stop') clearTimeout(rep);
+    
+    if (num>5) return msg.channel.send("no.");
 
     for(var i =0; i<num; i++) {
         var rep = setTimeout(function(){
